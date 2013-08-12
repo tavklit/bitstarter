9d487c8d11c5fd7397473ab007620d079df5dd29
tavklit <mail@tavklitgaard.dk>
2013-08-12 14:59:33 +0000

    changes

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = express();
 
-//app.use(express.logger());
+app.use(express.logger());
 
-//app.get('/', function(request, response) {
-//  response.send('Hello World2!');
-//});
+app.get('/', function(request, response) {
+  response.send('Hello World2!');
+});
 
+buf = new Buffer(256);
 fs.readFileSync('index.html');
 buf.toString();
 
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
