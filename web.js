f375157bbfad1462390bd95065025d98f7de932c
tavklit <mail@tavklitgaard.dk>
2013-08-12 14:02:43 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+//app.get('/', function(request, response) {
+//  response.send('Hello World2!');
+//});
+
+fs.readFileSync('index.html');
+buf.toString();
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
