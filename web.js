8391460f85be01952191966ac3eac70753806b1f
tavklit <mail@tavklitgaard.dk>
2013-08-12 15:03:15 +0000

    rollback

--- web.js
+++ web.js
@@ -7,9 +7,9 @@ app.get('/', function(request, response) {
   response.send('Hello World2!');
 });
 
-buf = new Buffer(256);
-fs.readFileSync('index.html');
-buf.toString();
+//buf = new Buffer(256);
+//fs.readFileSync('index.html');
+//buf.toString();
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
