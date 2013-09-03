74ab47f38896ee3fe2c95c53e2e85f8acbcd3ab1
tavklit <mail@tavklitgaard.dk>
2013-09-03 08:40:46 +0000

    added fluid and new nav

--- grader.js
+++ grader.js
@@ -26,14 +26,28 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var URL_DEFAULT = "http://www.google.com";
+var sys = require('util');
+var rest = require('restler');
+ 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1); 
+
+// http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+
+var cheerioUrl = function(url_result) {
+   return cheerio.load(fs.readFileSync(url_result));
+};
+
+var getUrl = function(url_result, checksfile) {
+$ = cheerioUrl(url_result);
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -55,6 +69,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrlResult = function(url_result, checksfile) {
+    $ = cheerioUrlResult(url_result);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
+    }
+    return out;
+};    
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,7 +90,26 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url_result>', 'Url from command line', URL_DEFAULT) 
         .parse(process.argv);
+    
+    if(program.url) {
+          rest.get(program.url).on('complete', function(result) {
+
+          var checkJson = getUrl(result, program.checks);
+          var outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+          
+        });
+
+}
+        else  {
+
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson); 
+              };
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
